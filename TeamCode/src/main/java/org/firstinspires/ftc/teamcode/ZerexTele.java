bf706e46dec0acdb45b492562b4ccca2eba32d7b
ZebraFTC <zebracompetitionteams@gmail.com>
2025-09-24 19:22:30 -0400

    Added kicker controls -Andrew

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZerexTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZerexTele.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 
@@ -16,6 +17,8 @@ public class ZerexTele extends LinearOpMode {
     public DcMotor Intake;
     public DcMotor OutRight;
     public DcMotor OutLeft;
+    public Servo Kicker;
+    double speed;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -27,6 +30,7 @@ public class ZerexTele extends LinearOpMode {
         Intake = hardwareMap.get(DcMotor.class, "intake");
         OutRight = hardwareMap.get(DcMotor.class, "rightOut");
         OutLeft = hardwareMap.get(DcMotor.class, "leftOut");
+        Kicker = hardwareMap.get(Servo.class,"kicker");
 
         waitForStart();
 
@@ -36,9 +40,9 @@ public class ZerexTele extends LinearOpMode {
             double turn = gamepad1.left_stick_y;
 
             FrontRight.setPower(drive + turn);
-            FrontLeft.setPower(drive - turn);
+            FrontLeft.setPower(-drive - turn);
             BackRight.setPower(drive + turn);
-            BackLeft.setPower(drive - turn);
+            BackLeft.setPower(-drive - turn);
 
             if (gamepad2.left_bumper) {
                 Intake.setPower(1);
@@ -46,17 +50,38 @@ public class ZerexTele extends LinearOpMode {
                 Intake.setPower(0);
             }
             if (gamepad2.right_bumper) {
-                OutRight.setPower(-1);
-                OutLeft.setPower(1);
+                OutRight.setPower(-speed);
+                OutLeft.setPower(speed);
             } else {
                 OutLeft.setPower(0);
                 OutRight.setPower(0);
             }
-            if (gamepad2.x) {
+            if (gamepad2.y) {
                 Intake.setPower(-1);
             }
             else {
                 Intake.setPower(0);
+            }
+            if (gamepad2.dpad_down){
+                speed = 0.25;
+            }
+            else if (gamepad2.dpad_left){
+                speed = 0.5;
+            }
+            else if (gamepad2.dpad_right){
+                speed = 0.75;
+            }
+            else if(gamepad1.dpad_up){
+                speed = 1;
+            }
+            else {
+                speed = 0;
+            }
+            if (gamepad2.a){
+                Kicker.setPosition(0.1);
+            }
+            if (gamepad2.b){
+                Kicker.setPosition(0.4);
             }
             }
          }
