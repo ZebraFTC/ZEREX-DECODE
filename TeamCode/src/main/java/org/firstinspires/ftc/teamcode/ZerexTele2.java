0f99dac719f304195b51e2f090de3578d2d7250c
ZLT06 <zebracompetitionteams@gmail.com>
2025-10-25 12:58:09 -0400

    Added strafe and adjusted for without shooter. -Andrew

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZerexTele2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZerexTele2.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -9,17 +8,17 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 
-public class ZerexTele extends LinearOpMode {
+public class ZerexTele2 extends LinearOpMode {
 
     public DcMotor FrontRight;
     public DcMotor FrontLeft;
     public DcMotor BackRight;
     public DcMotor BackLeft;
     public DcMotor Intake;
-    public DcMotor OutRight;
-    public DcMotor OutLeft;
+//  public DcMotor OutRight;
+//  public DcMotor OutLeft;
     public DcMotor TransferBelt;
-    public Servo Kicker;
+//    public Servo Kicker;
     double speed;
 
     @Override
@@ -30,26 +29,30 @@ public class ZerexTele extends LinearOpMode {
         BackRight = hardwareMap.get(DcMotor.class, "backRight");
         BackLeft = hardwareMap.get(DcMotor.class, "backLeft");
         Intake = hardwareMap.get(DcMotor.class, "intake");
-        OutRight = hardwareMap.get(DcMotor.class, "rightOut");
-        OutLeft = hardwareMap.get(DcMotor.class, "leftOut");
-        Kicker = hardwareMap.get(Servo.class,"kicker");
+        //OutRight = hardwareMap.get(DcMotor.class, "rightOut");
+        //OutLeft = hardwareMap.get(DcMotor.class, "leftOut");
         TransferBelt = hardwareMap.get(DcMotor.class, "belt");
 
+        //reverse wheel directions on the right
         FrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         BackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+         TransferBelt.setDirection(DcMotorSimple.Direction.REVERSE);
+
 
         waitForStart();
 
         while (opModeIsActive()) {
 
             double drive = 0.6 * gamepad1.left_stick_y;
-            double turn = -0.6 *  gamepad1.right_stick_x;
+            double turn = -0.6 * gamepad1.right_stick_x;
+            double strafe = 0.6 * gamepad1.left_stick_x;
 
-            FrontRight.setPower(drive - turn);
-            FrontLeft.setPower(drive + turn);
-            BackRight.setPower(drive - turn);
-            BackLeft.setPower(drive + turn);
+            FrontRight.setPower(drive - turn + strafe);
+            FrontLeft.setPower(drive + turn - strafe);
+            BackRight.setPower(drive - turn - strafe);
+            BackLeft.setPower(drive + turn + strafe);
 
+            //intake
             if (gamepad2.left_bumper) {
                 Intake.setPower(1);
                 TransferBelt.setPower(1);
@@ -57,44 +60,54 @@ public class ZerexTele extends LinearOpMode {
                 Intake.setPower(0);
                 TransferBelt.setPower(0);
             }
-            if (gamepad2.right_bumper) {
-                OutRight.setPower(-speed);
-                OutLeft.setPower(speed);
-            } else {
-                OutLeft.setPower(0);
-                OutRight.setPower(0);
-            }
+
+            //shooter
+            // if (gamepad2.right_bumper) {
+            //  OutRight.setPower(-speed);
+            //  OutLeft.setPower(speed);
+            // }
+            //  else {
+            // OutLeft.setPower(0);
+            // OutRight.setPower(0);
+            //}
+
+            //reverse intake
             if (gamepad2.y) {
                 Intake.setPower(-1);
                 TransferBelt.setPower(-1);
-            }
-            else {
+            } else {
                 Intake.setPower(0);
                 TransferBelt.setPower(0);
             }
-            if (gamepad2.dpad_down){
+
+            //25% speed
+            if (gamepad2.dpad_down) {
                 speed = 0.25;
             }
-            else if (gamepad2.dpad_left){
+            //50% speed
+            else if (gamepad2.dpad_left) {
                 speed = 0.5;
             }
-            else if (gamepad2.dpad_right){
+            //75% speed
+            else if (gamepad2.dpad_right) {
                 speed = 0.75;
             }
-            else if(gamepad2.dpad_up){
+            //100% speed
+            else if (gamepad2.dpad_up) {
                 speed = 1;
             }
-            e lse {
-                speed = 0.4;
-            }
-            if (gamepad2.a){
-                Kicker.setPosition(0.7); //up position
-            }
+            //45% speed
             else {
-                Kicker.setPosition(.95); //down position
+                speed = 0.45;
             }
 
+            if (gamepad2.a) {
+//                Kicker.setPosition(0.7); // kicker up position
+            } else {
+//                Kicker.setPosition(.95); //kicker down position
             }
-         }
+
+        }
     }
+}
 
