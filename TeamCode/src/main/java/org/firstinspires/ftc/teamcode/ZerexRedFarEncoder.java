dbb5073c37517bb429ed38c90598069dcb4ce508
ZebraFTC <zebracompetitionteams@gmail.com>
2026-01-03 10:57:11 -0500

    new thing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ZerexRedFarEncoder.java
@@ -0,0 +1,101 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+@Autonomous
+
+public class ZerexRedFarEncoder extends LinearOpMode {
+
+    public DcMotor FrontRight;
+    public DcMotor FrontLeft;
+    public DcMotor BackRight;
+    public DcMotor BackLeft;
+    public DcMotor Intake;
+    public DcMotor Shooter;
+    public DcMotor Transfer;
+    private int FrontRightPosition;
+    private int FrontLeftPosition;
+    private int BackRightPosition;
+    private int BackLeftPosition;
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+
+        FrontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        FrontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        BackRight = hardwareMap.get(DcMotor.class, "backRight");
+        BackLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        Intake = hardwareMap.get(DcMotor.class, "intake");
+        Shooter = hardwareMap.get(DcMotor.class, "shooter");
+        Transfer = hardwareMap.get(DcMotor.class, "belt");
+
+        FrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        BackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        Transfer.setDirection(DcMotorSimple.Direction.REVERSE);
+        Shooter.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        FrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+        FrontRightPosition = 0;
+        FrontLeftPosition = 0;
+        BackRightPosition = 0;
+        BackLeftPosition = 0;
+
+        waitForStart();
+// 30 ticks ~ an inch
+        drive(360,360,360,360,0.5);
+        shoot(0.7,3000);
+        drive(-200,200,-200,200,0.5);
+
+    }
+
+    public void drive(int FrontRightTarget, int FrontLeftTarget, int BackRightTarget, int BackLeftTarget, double speed){
+        FrontRightPosition += FrontRightTarget;
+        FrontLeftPosition += FrontLeftTarget;
+        BackRightPosition += BackRightTarget;
+        BackLeftPosition += BackLeftTarget;
+
+        FrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        FrontRight.setTargetPosition(FrontRightPosition);
+        FrontLeft.setTargetPosition(FrontLeftPosition);
+        BackRight.setTargetPosition(BackRightPosition);
+        BackLeft.setTargetPosition(BackLeftPosition);
+
+        FrontRight.setPower(speed);
+        FrontLeft.setPower(speed);
+        BackRight.setPower(speed);
+        BackLeft.setPower(speed);
+
+
+        while (opModeIsActive() && FrontRight.isBusy() || FrontLeft.isBusy() || BackRight.isBusy() || BackLeft.isBusy()) {
+            idle();
+        }
+
+        FrontRight.setPower(0);
+        FrontLeft.setPower(0);
+        BackRight.setPower(0);
+        BackLeft.setPower(0);
+    }
+
+    public void shoot(double power, long time){
+        Shooter.setPower(power);
+        sleep(2500);
+        Intake.setPower(1.0);
+        Transfer.setPower(1.0);
+        sleep(time);
+        Shooter.setPower(0);
+        Intake.setPower(0);
+        Transfer.setPower(0);
+    }
+
+}
